ba3a407c7f677719b7465f05e85bd34f777b4a95
Anas Mirza <anasmirza534@gmail.com>
2026-08-30 17:28:31 +0530

    added error pages and log status code

--- main.go
+++ main.go
@@ -13,8 +13,11 @@ import (
 func main() {
 	godotenv.Load()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", withLogging(homeHandler))
+	http.Handle("/static/", http.StripPrefix(
+		"/static/",
+		http.FileServer(http.Dir("static")),
+	))
+	http.HandleFunc("/", withLogging(handleNotFound))
 	http.HandleFunc("/home", withLogging(homeHandler))
 	http.HandleFunc("GET /post/{id}", withLogging(postDetailHandler))
 
@@ -25,18 +28,55 @@ func main() {
 	}
 }
 
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (r *statusRecorder) WriteHeader(status int) {
+	r.status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
 func withLogging(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		rec := &statusRecorder{ResponseWriter: w, status: http.StatusOK}
 		start := time.Now()
-		next(w, r)
-		log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+		next(rec, r)
+		log.Printf("%s %s %d %v", r.Method, r.URL.Path, rec.status, time.Since(start))
 	}
+}
+
+func handleNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+
+	tmpl := template.Must(template.ParseFiles(
+		"templates/base.html",
+		"templates/not-found.html",
+	))
+	tmpl.ExecuteTemplate(w, "base", nil)
+}
+
+func renderError(w http.ResponseWriter, code int, message string) {
+	tmpl := template.Must(template.ParseFiles(
+		"templates/base.html",
+		"templates/error.html",
+	))
+
+	w.WriteHeader(code)
+
+	tmpl.ExecuteTemplate(w, "base", struct {
+		Code    int
+		Message string
+	}{code, message})
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	list, err := reddit.GetHome()
 	if err != nil {
 		log.Println(err)
+		renderError(w, http.StatusInternalServerError, "Could not load reddit")
+		return
 	}
 
 	tmpl := template.Must(template.ParseFiles(
@@ -51,6 +91,8 @@ func postDetailHandler(w http.ResponseWriter, r *http.Request) {
 	detail, err := reddit.GetPost(postId)
 	if err != nil {
 		log.Println(err)
+		renderError(w, http.StatusInternalServerError, "Could not load reddit")
+		return
 	}
 
 	tmpl := template.Must(template.ParseFiles(
